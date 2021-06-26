66a67042965ba4ad3d0a8725a79204cf530893ff
Nadji <nadjitan@yahoo.com.ph>
2021-06-26 08:53:26 +0800

    Remove unused import in UserController

--- src/main/java/com/tunnelnetwork/KpOnlineStore/Controllers/UserController.java
+++ src/main/java/com/tunnelnetwork/KpOnlineStore/Controllers/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
