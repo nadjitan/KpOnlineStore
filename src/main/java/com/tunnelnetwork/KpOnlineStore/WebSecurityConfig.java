00bc70a43017894bbefb0053c8904ae97e0a3381
Nadji <nadjitan@yahoo.com.ph>
2021-07-02 17:38:33 +0800

    html links to html button

--- src/main/java/com/tunnelnetwork/KpOnlineStore/WebSecurityConfig.java
+++ src/main/java/com/tunnelnetwork/KpOnlineStore/WebSecurityConfig.java
@@ -36,6 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     http
       .authorizeRequests()
         .antMatchers(
+          "/contact-us/**",
+          "/about-us/**",
           "/store/**",
           "/profile/**",
           "/cart/**",
