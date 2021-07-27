cb1ec25349289c7111d0a08369bc662096ed188f
Nadji <nadjitan@yahoo.com.ph>
2021-07-27 12:33:11 +0800

    Fix input quantity

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -109,7 +109,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const statusText = $(".product-status").text();
     $(".product-status").text(statusText.replace(/([A-Z])/g, " $1").trim());
 
-    console.log($(".comment-time").text());
+    $('#product-quantity').keypress(function (e) {
+      const keyCode = e.which;
+      /*
+       8 - (backspace)
+       32 - (space)
+       48-57 - (0-9)Numbers
+     */
+
+      if ((keyCode != 8 || keyCode == 32) && (keyCode < 48 || keyCode > 57)) {
+        return false;
+      }
+
+      else if (this.value.length === 2) {
+        return false;
+      }
+    });
 
     $(".comment-time").each(function() {
       const commentTime = new Date($(this).text());
